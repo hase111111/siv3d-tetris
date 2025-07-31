30015631ddab44085e221dadf446acc47253203d
Hasegawa Taisei <hasehasehase61@gmail.com>
2025-08-01 00:38:35 +0900

    feat: Titleシーンの追加

--- DxLib/Source/title_scene.cpp
+++ DxLib/Source/title_scene.cpp
@@ -1,11 +1,11 @@
 
-//! @file debug_scene.cpp
+//! @file title_scene.cpp
 //! @brief
 //! Copyright(c) 2024-2025 Taisei Hasegawa
 //! Released under the MIT license
 //! https://opensource.org/licenses/mit-license.php
 
-#include "debug_scene.h"
+#include "title_scene.h"
 
 #include <DxLib.h>
 
@@ -13,7 +13,7 @@
 
 namespace mytetris {
 
-DebugScene::DebugScene(
+TitleScene::TitleScene(
     const std::shared_ptr<SceneChangeListener>& scene_change_listener_ptr,
     const std::shared_ptr<const DxLibKeyboard>& dxlib_keyboard_ptr)
     : scene_change_listener_ptr_(scene_change_listener_ptr),
@@ -22,25 +22,24 @@ DebugScene::DebugScene(
   ASSERT_NOT_NULL_PTR(dxlib_keyboard_ptr);
 }
 
-bool DebugScene::Update() {
+bool TitleScene::Update() {
   if (dxlib_keyboard_ptr_->GetPressingCount(KeyHandle::kEscape) == 1) {
     return false;
   }
 
-  if (dxlib_keyboard_ptr_->GetPressingCount(KeyHandle::kT) == 1) {
-    scene_change_listener_ptr_->RequestAddScene(SceneName::kTitle,
-                                                SceneChangeParameter{});
+  if (dxlib_keyboard_ptr_->GetPressingCount(KeyHandle::kC) == 1) {
+    scene_change_listener_ptr_->RequestDeleteScene(1, SceneChangeParameter{});
   }
 
   return true;
 }
 
-void DebugScene::Draw() const {
-  DrawString(0, 0, "Debug Scene", GetColor(255, 255, 255));
+void TitleScene::Draw() const {
+  DrawString(0, 0, "Title Scene", GetColor(255, 255, 255));
 }
 
-void DebugScene::OnStart(const SceneChangeParameter&) {}
+void TitleScene::OnStart(const SceneChangeParameter&) {}
 
-void DebugScene::OnReturnFromOtherScene(const SceneChangeParameter&) {}
+void TitleScene::OnReturnFromOtherScene(const SceneChangeParameter&) {}
 
 }  // namespace mytetris
