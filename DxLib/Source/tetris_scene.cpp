95e2b4af646a946730aaed8fe20b69913d702992
hase111111 <hasehasehase61@gmail.com>
2025-08-04 14:15:56 +0900

    feat: Tetrisシーンの追加

---
+++ DxLib/Source/tetris_scene.cpp
@@ -0,0 +1,29 @@
+
+//! @file tetris_scene.cpp
+//! @brief
+//! Copyright(c) 2024-2025 Taisei Hasegawa
+//! Released under the MIT license
+//! https://opensource.org/licenses/mit-license.php
+
+#include "tetris_scene.h"
+
+namespace mytetris {
+
+TetrisScene::TetrisScene(
+    const std::shared_ptr<SceneChangeListener>& scene_change_listener_ptr,
+    const std::shared_ptr<const DxLibKeyboard>& dxlib_keyboard_ptr,
+    const std::shared_ptr<const ResourceContainer>& resource_container_ptr)
+    : scene_change_listener_ptr_(scene_change_listener_ptr),
+      dxlib_keyboard_ptr_(dxlib_keyboard_ptr),
+      resource_container_ptr_(resource_container_ptr) {}
+
+bool TetrisScene::Update() { return true; }
+
+void TetrisScene::Draw() const {}
+
+void TetrisScene::OnStart(const SceneChangeParameter& parameter) {}
+
+void TetrisScene::OnReturnFromOtherScene(
+    const SceneChangeParameter& parameter) {}
+
+}  // namespace mytetris
--- DxLib/Source/tetris_scene.h
+++ DxLib/Source/tetris_scene.h
@@ -1,1 +1,41 @@
+
+//! @file tetris_scene.h
+//! @brief
+//! Copyright(c) 2024-2025 Taisei Hasegawa
+//! Released under the MIT license
+//! https://opensource.org/licenses/mit-license.php
+
 #pragma once
+
+#include <memory>
+
+#include "dxlib_keyboard.h"
+#include "i_scene.h"
+#include "resource_container.h"
+#include "scene_change_listener.h"
+
+namespace mytetris {
+
+class TetrisScene final : public IScene {
+ public:
+  TetrisScene(
+      const std::shared_ptr<SceneChangeListener>& scene_change_listener_ptr,
+      const std::shared_ptr<const DxLibKeyboard>& dxlib_keyboard_ptr,
+      const std::shared_ptr<const ResourceContainer>& resource_container_ptr);
+  ~TetrisScene() = default;
+
+  bool Update() override;
+
+  void Draw() const override;
+
+  void OnStart(const SceneChangeParameter& parameter) override;
+
+  void OnReturnFromOtherScene(const SceneChangeParameter& parameter) override;
+
+ private:
+  const std::shared_ptr<SceneChangeListener> scene_change_listener_ptr_;
+  const std::shared_ptr<const DxLibKeyboard> dxlib_keyboard_ptr_;
+  const std::shared_ptr<const ResourceContainer> resource_container_ptr_;
+};
+
+}  // namespace mytetris
