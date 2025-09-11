b74f783c2804f916d79f2b4a567ce423dcf37fe2
hase111111 <hasehasehase61@gmail.com>
2025-09-11 15:56:41 +0900

    add: Siv3Dのプロジェクトを追加

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 //! Released under the MIT license
 //! https://opensource.org/licenses/mit-license.php
 
+#if defined DXLIB_COMPILE
 #include <DxLib.h>
 
 #include "error_box.h"
@@ -30,3 +31,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
   return 0;
 }
+
+#elif defined SIV3D_COMPILE
+
+#include <Siv3D.hpp>
+
+#include "system_main.h"
+
+static void Main() {
+  using mytetris::SystemMain;
+
+  SystemMain system_main;
+
+  if (system_main.Initialize()) {
+    system_main.Main();
+    system_main.Finalize();
+  } else {
+  }
+}
+
+#endif
