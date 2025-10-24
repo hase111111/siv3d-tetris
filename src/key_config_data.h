db42a77d4479b842c229690d7767664ab4c78acc
Hasegawa Taisei <hasehasehase61@gmail.com>
2025-10-24 22:41:57 +0900

    update: KeyConfigの追加

---
+++ src/key_config_data.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "key_handle.h"
+
+namespace mytetris {
+
+struct KeyConfigData {
+ public:
+  KeyConfigData()
+      : move_left_key_(KeyHandle::kLeft),
+        move_right_key_(KeyHandle::kRight),
+        soft_drop_key_(KeyHandle::kDown),
+        hard_drop_key_(KeyHandle::kUp),
+        rotate_cw_key_(KeyHandle::kD),
+        rotate_ccw_key_(KeyHandle::kA),
+        hold_key_(KeyHandle::kW),
+        pause_key_(KeyHandle::kP) {}
+  KeyHandle move_left_key_;
+  KeyHandle move_right_key_;
+  KeyHandle soft_drop_key_;
+  KeyHandle hard_drop_key_;
+  KeyHandle rotate_cw_key_;
+  KeyHandle rotate_ccw_key_;
+  KeyHandle hold_key_;
+  KeyHandle pause_key_;
+};
+
+}  // namespace mytetris
--- src/key_event_handler.h
+++ src/key_event_handler.h
@@ -9,42 +9,9 @@
 
 #include <array>
 
-namespace mytetris {
+#include "key_handle.h"
 
-enum class KeyHandle : int {
-  kEscape = 0x01,
-  kA = 0x1E,
-  kB = 0x30,
-  kC = 0x2E,
-  kD = 0x20,
-  kE = 0x12,
-  kF = 0x21,
-  kG = 0x22,
-  kH = 0x23,
-  kI = 0x17,
-  kJ = 0x24,
-  kK = 0x25,
-  kL = 0x26,
-  kM = 0x32,
-  kN = 0x31,
-  kO = 0x18,
-  kP = 0x19,
-  kQ = 0x10,
-  kR = 0x13,
-  kS = 0x1F,
-  kT = 0x14,
-  kU = 0x16,
-  kV = 0x2F,
-  kW = 0x11,
-  kX = 0x2D,
-  kY = 0x15,
-  kZ = 0x2C,
-  kSpace = 0x039,
-  kLeft = 0xCB,
-  kRight = 0xCD,
-  kUp = 0xC8,
-  kDown = 0xD0,
-};
+namespace mytetris {
 
 //! @brief DxLibのキーボード入力を取得するクラス．
 class KeyEventHandler final {
